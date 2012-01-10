b3d496dbc06ba56f9cff4a23d733129d544bcb73
Josh Rolstad <jrolstad@gmail.com>
2012-01-10 07:50:38 -0800

    Adding Nuget packaging specification

---
+++ Directus.SimpleDb/Factories/DomainRequestFactory.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.SimpleDB.Model;
+
+namespace Directus.SimpleDb.Factories
+{
+    /// <summary>
+    /// Factory for creating a domain request
+    /// </summary>
+    public class DomainRequestFactory
+    {
+        /// <summary>
+        /// Creates a request to create a domain
+        /// </summary>
+        /// <param name="domainName"></param>
+        /// <returns></returns>
+        public CreateDomainRequest Create(string domainName)
+        {
+            return new CreateDomainRequest().WithDomainName(domainName);
+        }
+    }
+}
